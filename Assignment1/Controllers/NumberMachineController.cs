1be389504eb620232111c55151f9a581b24bb57f
ehndee <andyndv@gmail.com>
2022-12-06 17:24:56 -0500

    Update

---
+++ Assignment1/Controllers/NumberMachineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1.Controllers
+{
+    public class NumberMachineController : ApiController
+    {
+
+        public int Get(int id)
+        {
+
+            int product = id * id - 24 + 99 / 2;
+            return product;
+        }
+    }
+}
---
+++ Assignment1/Controllers/SquareController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1.Controllers
+{
+    public class SquareController : ApiController
+    {
+
+        public int Get(int id)
+        {
+            int squared = id * id;
+            return squared;
+        }
+    }
+}
