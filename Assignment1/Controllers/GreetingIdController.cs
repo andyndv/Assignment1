541c8b314372aac4669c1c4d5e853c26086e45cf
ehndee <andyndv@gmail.com>
2022-12-06 17:25:37 -0500

    update

---
+++ Assignment1/Controllers/GreetingIdController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1.Controllers
+{
+    public class GreetingIdController : ApiController
+    {
+        public string Get(int id)
+        {
+            string greeting = "Greetings to " + id + " people";
+            return greeting;
+        }
+    }
+}
